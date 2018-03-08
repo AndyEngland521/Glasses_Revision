06f1754e4f93db4735fb5e400d0ef181af80f24b
AndyEngland521 <andrew.england@sparkfun.com>
2018-03-07 17:36:18 -0700

    cleaned ARTNET code

--- Code/RezzBrain/RezzBrain.ino
+++ Code/RezzBrain/RezzBrain.ino
@@ -65,30 +65,6 @@ boolean ConnectWifi(void)
   return state;
 }
 
-void initTest()
-{
-  for (int i = 0 ; i < numLeds ; i++){
-    leds[i] = (127, 0, 0);
-  }
-    FastLED.show();
-    delay(500);
-  for (int i = 0 ; i < numLeds ; i++){
-    leds[i] = (0, 127, 0);
-  }
-    FastLED.show();
-    delay(500);
-  for (int i = 0 ; i < numLeds ; i++){
-    leds[i] = (0, 127, 0);
-  }
-    FastLED.show();
-    delay(500);
-  for (int i = 0 ; i < numLeds ; i++){
-    leds[i] = (0, 0, 0);
-  }
-    FastLED.show();
-    delay(500);
-}
-
 void onDmxFrame(uint16_t universe, uint16_t length, uint8_t sequence, uint8_t* data)
 {
   sendFrame = 1;
@@ -99,20 +75,6 @@ void onDmxFrame(uint16_t universe, uint16_t length, uint8_t sequence, uint8_t* d
     FastLED.show();
   }
 
-  // Store which universe has got in
-  if ((universe - startUniverse) <= maxUniverses)
-    universesReceived[universe - startUniverse] = 1;
-
-  for (int i = 0 ; i < maxUniverses ; i++)
-  {
-    if (universesReceived[i] == 0)
-    {
-      Serial.println("Broke");
-      sendFrame = 0;
-      break;
-    }
-  }
-
   // read universe and put into the right part of the display buffer
   for (int i = 0; i < length / 3; i++)
   {
@@ -121,13 +83,7 @@ void onDmxFrame(uint16_t universe, uint16_t length, uint8_t sequence, uint8_t* d
       leds[led] = CRGB(data[i * 3], data[i * 3 + 1], data[i * 3 + 2]);
   }
   previousDataLength = length;     
-  
-  if (sendFrame)
-  {
-    FastLED.show();
-    // Reset universeReceived to 0
-    memset(universesReceived, 0, maxUniverses);
-  }
+  FastLED.show();
 }
 
 void setup()
